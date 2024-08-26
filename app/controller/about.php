843e8a3a9961c017207ae75970300e3f3faee5c7
= <=>
2024-08-27 00:28:17 +0800

    push

--- app/controller/about.php
+++ app/controller/about.php
@@ -1,6 +1,10 @@
 <?php
 
 class About {
+    public function index($name = "afiq", $passion = "php developer") {
+        echo "Hi, my name is $name. I'm passion in $passion";
+    }
+
     public function page() {
         echo "about/page";
     }
