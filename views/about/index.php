1324e4b6481990c776ed4b67b7ab9aa9383289c3
Muhamad Afiq <93767398+fyyqq@users.noreply.github.com>
2024-08-27 17:06:46 +0800

    add views

---
+++ views/about/index.php
@@ -0,0 +1,11 @@
+<h2>About Index</h2>
+<table>
+    <tr>
+        <th>name</th>
+        <th>passion</th>
+    </tr>
+    <tr>
+        <td><?php echo $data['name']; ?></td>
+        <td><?php echo $data['passion']; ?></td>
+    </tr>
+</table>
